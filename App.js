d51e48e6d79244450848a7d803c9a0f669772a0d
shafdo <yugantha1468@gmail.com>
2023-02-03 19:08:01 +0530

    Version 0.0.9

--- App.js
+++ App.js
@@ -1,16 +1,9 @@
+import React, { useCallback, useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import AppNavigationContainer from './src/navigations';
+import * as SplashScreen from 'expo-splash-screen';
 
 export default function App() {
   return <AppNavigationContainer></AppNavigationContainer>;
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center'
-//   }
-// });
